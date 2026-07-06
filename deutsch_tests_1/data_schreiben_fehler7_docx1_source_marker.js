5bdbc291097aeee4cfd915f5f63ac6ead809896d
cagdascagdas100 <cagdascagdas100@proton.me>
2026-07-06 20:23:58 +0300

    Mark Schreiben Fehler 7 source as uploaded docx1

---
+++ deutsch_tests_1/data_schreiben_fehler7_docx1_source_marker.js
@@ -0,0 +1,14 @@
+(function(){
+'use strict';
+var key='sf2_7';
+if(window.DEUTSCH_TESTS&&window.DEUTSCH_TESTS[key]){
+  window.DEUTSCH_TESTS[key].source='7. Sprach Fehlern(1).docx';
+  window.DEUTSCH_TESTS[key].sourceVerifiedWithLatestUpload=true;
+}
+if(window.DEUTSCH_LESSONS&&window.DEUTSCH_LESSONS[key]){
+  window.DEUTSCH_LESSONS[key].source='7. Sprach Fehlern(1).docx';
+  window.DEUTSCH_LESSONS[key].longSourceVerified=true;
+  window.DEUTSCH_LESSONS[key].minimumWords='1500+';
+}
+window.AAYS_SF2_7_SOURCE_DOCX1_OK=true;
+})();
