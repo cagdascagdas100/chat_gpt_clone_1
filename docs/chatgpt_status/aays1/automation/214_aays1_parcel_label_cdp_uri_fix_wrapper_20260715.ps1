ca8add89647012c78214caaf539f18e2df4cb78a
cagdascagdas100 <cagdascagdas100@proton.me>
2026-07-15 19:12:05 +0300

    Add CDP websocket retry to Parcel Label Task 214

--- docs/chatgpt_status/aays1/automation/214_aays1_parcel_label_cdp_uri_fix_wrapper_20260715.ps1
+++ docs/chatgpt_status/aays1/automation/214_aays1_parcel_label_cdp_uri_fix_wrapper_20260715.ps1
@@ -6,13 +6,35 @@ $sourcePath = Join-Path $repoRoot 'docs\chatgpt_status\aays1\automation\214_aays
 if (-not (Test-Path -LiteralPath $sourcePath -PathType Leaf)) { throw "TASK_214_SOURCE_NOT_FOUND: $sourcePath" }
 $text = Get-Content -LiteralPath $sourcePath -Raw -Encoding UTF8
 $oldTarget = '$target = @($targets | Where-Object { $_.type -eq ''page'' -and $_.webSocketDebuggerUrl } | Select-Object -First 1)'
-$newTarget = '$target = $targets | Where-Object { $_.type -eq ''page'' -and $_.webSocketDebuggerUrl } | Select-Object -First 1'
+$newTarget = @'
+$targetCandidates = @($targets | Where-Object { $_.type -eq 'page' -and $_.webSocketDebuggerUrl })
+$target = $targetCandidates | Where-Object { [string]$_.url -like '*TerraYield_England_Program_Parcel_Layer_Matrix*' } | Select-Object -First 1
+if (-not $target) { $target = $targetCandidates | Select-Object -First 1 }
+'@
 $oldConnect = '$socket.ConnectAsync([Uri]$target.webSocketDebuggerUrl,[Threading.CancellationToken]::None).GetAwaiter().GetResult()'
-$newConnect = '$socket.ConnectAsync([Uri]([string]$target.webSocketDebuggerUrl),[Threading.CancellationToken]::None).GetAwaiter().GetResult()'
+$newConnect = @'
+$cdpUri = [Uri]([string]$target.webSocketDebuggerUrl)
+$connectDeadline = (Get-Date).AddSeconds(20)
+$connected = $false
+$lastConnectError = ''
+do {
+  try {
+    if ($socket) { try { $socket.Dispose() } catch {} }
+    $socket = [Net.WebSockets.ClientWebSocket]::new()
+    $socket.ConnectAsync($cdpUri,[Threading.CancellationToken]::None).GetAwaiter().GetResult()
+    $connected = ($socket.State -eq [Net.WebSockets.WebSocketState]::Open)
+  } catch {
+    $lastConnectError = $_.Exception.Message
+    if ((Get-Date) -ge $connectDeadline) { break }
+    Start-Sleep -Milliseconds 300
+  }
+} while (-not $connected -and (Get-Date) -lt $connectDeadline)
+if (-not $connected) { throw ('CDP_PAGE_WEBSOCKET_CONNECT_TIMEOUT: ' + $lastConnectError) }
+'@
 if (-not $text.Contains($oldTarget)) { throw 'TASK_214_TARGET_PATCH_PATTERN_NOT_FOUND' }
 if (-not $text.Contains($oldConnect)) { throw 'TASK_214_CONNECT_PATCH_PATTERN_NOT_FOUND' }
 $patched = $text.Replace($oldTarget,$newTarget).Replace($oldConnect,$newConnect)
-$tempPath = Join-Path ([IO.Path]::GetTempPath()) ('aays_task214_uri_fix_' + [guid]::NewGuid().ToString('N') + '.ps1')
+$tempPath = Join-Path ([IO.Path]::GetTempPath()) ('aays_task214_cdp_retry_' + [guid]::NewGuid().ToString('N') + '.ps1')
 try {
   [IO.File]::WriteAllText($tempPath,$patched,[Text.UTF8Encoding]::new($false))
   $tokens=$null;$errors=$null
